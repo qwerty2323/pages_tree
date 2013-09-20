aa3bfb79203100b807f8a39902e2132688c0086e
Alex G <fotonstep@MacBook-Pro-Alex.local>
2013-09-20 21:56:39 +0600

    [upd] content formatting

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.14'
+gem 'rails', '3.1'
 gem 'sqlite3'
 gem 'jquery-rails'
 gem 'execjs', '1.4.0' # due to JSON bug
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,32 @@
+# encoding: utf-8
 class Page < ActiveRecord::Base
   has_ancestry cache_depth:true, orphan_strategy: :rootify
   attr_accessible :slug, :title, :content, :ancestry
+  validates :slug, format:{with:/[\wа-яА-Я]+/i, message:'Chars, digits and underscore only'}
 
+  before_save :setup_before_save
+
+
+
+  def formatted_content
+    return '' unless self.content
+    self.content.gsub!(/\<b\>(.+?)\<\/b\>/im){"**#{$1}**"} # <b>[строка]</b> => **[строка]**
+    self.content.gsub!(/\<i\>(.+?)\<\/i\>/im){"\\\\#{$1}\\\\"} # <i>[строка]</i> => \\[строка]\\
+    self.content.gsub!(/\<a\s+?href="\/([\wа-яА-Я\/]+?)"\>(.+?)\<\/a\>/im){"((#{$1} #{$2}))"} # <a href="/name1/name2/name3">[строка]</a> => ((name1/name2/name3 [строка]))
+  end
   def to_param
     self.parent ? "#{self.parent.to_param}/#{self.slug}" : self.slug
+  end
+
+
+
+  private
+
+  def setup_before_save
+    if self.content
+      self.content.gsub!(/\*\*(.+?)\*\*/im){"<b>#{$1}</b>"} # **[строка]** => <b>[строка]</b>
+      self.content.gsub!(/\\\\(.+?)\\\\/im){"<i>#{$1}</i>"} # \\[строка]\\ => <i>[строка]</i>
+      self.content.gsub!(/\(\(([\wа-яА-Я\/]+?)\s(.+?)\)\)/im){"<a href=\"/#{$1}\">#{$2}</a>"} # ((name1/name2/name3 [строка])) => <a href="/name1/name2/name3">[строка]</a>
+    end
   end
 end

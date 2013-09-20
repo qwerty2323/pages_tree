750a534decd1ee0043f000d16392d1ca26566c37
Alex G <fotonstep@MacBook-Pro-Alex.local>
2013-09-20 17:00:22 +0600

    [init]

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'haml-rails'
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  gem 'database_cleaner'
+  gem 'rspec-rails'
+end
+
+gem 'ancestry'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PagesTree::Application.config.secret_token = '6f72eca44ae00fd84803c81e13944b0928bed9a06d5725b1825ae142634f19456060504a9c30c3acbfc025f7b6456a311eca1882c0d2630e0a16f584c0475965'
